2ad4ed5e936343e9f44f8dbb33228e39f5715cb5
Johannes Koester <johannes.koester@uni-due.de>
2025-10-21 12:25:59 -0300

    todo

--- denbi/99_koesterlab_cebitec.sh
+++ denbi/99_koesterlab_cebitec.sh
@@ -2,7 +2,6 @@
 setup_config ~/.config/snakemake/default/config.yaml "storage-s3-endpoint-url: https://openstack.cebitec.uni-bielefeld.de:8080\ndefault-storage-provider: s3\nsoftware-deployment-method: conda\ndefault-resources: []"
 
 setup_cmd snakemake "pixi global install snakemake --with snakemake-storage-plugin-s3"
-setup_cmd s5cmd
 
 DEPLOY_PROFILES=("common/98_koesterlab_common.sh" "denbi/99_koesterlab_cebitec.sh")
 
@@ -11,4 +10,6 @@ show_login_message \
   "Do not compute on the machine main. Instead, jump to compute01." \
   "Put any workflows in project specific subfolders under /mnt/workspace/<project>/ on compute01." \
   "Do NOT put non-public human or personal data anywhere in here (also not into the s3)!"
+
+# TODO add rclone setup and instructions
 
